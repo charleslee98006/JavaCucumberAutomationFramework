a25ecd0b67b57ef3d410670ded5dfbe333f120d8
charles.lee <charles.lee@slalom.com>
2020-05-14 11:11:13 -0400

    Attached the firefox drivers for windows and Mac and successfully display the automated browsers.

--- src/main/java/setup/BaseSetup.java
+++ src/main/java/setup/BaseSetup.java
@@ -5,15 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+/**
+ * This class sets up and configures selenium and rest assured for their respective tests.
+ */
 public class BaseSetup {
 
     private WebDriver webDriver;
 
+    /**
+     * This constructor calls on the configuration methods.
+     */
     public BaseSetup(){
         setUpWebDriver();
     }
 
+    /**
+     * This method sets up the webdrivers. Currently only supporting Chrome and Firefox for both windows and MacOS.
+     */
     public void setUpWebDriver(){
         String browser = System.getProperty("browser");
         if(("chrome").equalsIgnoreCase(browser)){
@@ -27,9 +37,15 @@ public class BaseSetup {
             ChromeDriverService driverService = ChromeDriverService.createDefaultService();
              webDriver = new ChromeDriver(driverService, options);
         }
-//        else if(("firefox").equalsIgnoreCase(browser)){
-//
-//        }
+        else if(("firefox").equalsIgnoreCase(browser)){
+            if(OS.isFamilyWindows()) {
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
+            }
+            else{
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver");
+            }
+            webDriver = new FirefoxDriver();
+        }
         else{
             try {
                 throw new Exception("Currently only support Chrome and Firefox.");
@@ -39,7 +55,10 @@ public class BaseSetup {
         }
         webDriver.manage().window().maximize();
     }
-
+    /**
+     * This method gets the configured webdriver.
+     * @return Webdriver - the webdriver currently being used.
+     */
     public WebDriver getWebDriver(){
         return this.webDriver;
     }
