ad63c99648e2d72d9186d501e5912a031f25b479
charles.lee <charles.lee@slalom.com>
2020-05-14 10:47:03 -0400

    Attached the chromedrivers for windows and Mac and successfully display the automated browsers.

--- src/main/java/setup/BaseSetup.java
+++ src/main/java/setup/BaseSetup.java
@@ -1,8 +1,46 @@
 package setup;
 
+import org.apache.commons.exec.OS;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class BaseSetup {
 
+    private WebDriver webDriver;
+
     public BaseSetup(){
-        System.out.println("Coming through here");
+        setUpWebDriver();
+    }
+
+    public void setUpWebDriver(){
+        String browser = System.getProperty("browser");
+        if(("chrome").equalsIgnoreCase(browser)){
+            if(OS.isFamilyWindows()) {
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+            }
+            else{
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+            }
+            ChromeOptions options = new ChromeOptions();
+            ChromeDriverService driverService = ChromeDriverService.createDefaultService();
+             webDriver = new ChromeDriver(driverService, options);
+        }
+//        else if(("firefox").equalsIgnoreCase(browser)){
+//
+//        }
+        else{
+            try {
+                throw new Exception("Currently only support Chrome and Firefox.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        webDriver.manage().window().maximize();
+    }
+
+    public WebDriver getWebDriver(){
+        return this.webDriver;
     }
 }
--- src/main/java/stepDefinitions/GUIStepDefs.java
+++ src/main/java/stepDefinitions/GUIStepDefs.java
@@ -1,16 +1,21 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
 import setup.BaseSetup;
 
 public class GUIStepDefs {
     private BaseSetup baseSetup;
+    private WebDriver webDriver;
 
     public GUIStepDefs(BaseSetup baseSetup){
         this.baseSetup = baseSetup;
+        webDriver = baseSetup.getWebDriver();
+
     }
 
     @Given("I land on {string} page")
     public void i_land_on_page(String url){
+
     }
 }
